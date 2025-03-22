05bbdf14cc8202bf33f5904e180632249913d3db
MABD <mabd.universe@gmail.com>
2025-03-22 21:57:08 +0200

    properly format task creation date

--- internal/renderer/tasksRenderer.go
+++ internal/renderer/tasksRenderer.go
@@ -14,7 +14,38 @@ func RenderTable(tasks []models.Task) {
 	table.SetHeaders("#", "Name", "Description", "Status", "Created At")
 
 	for _, task := range tasks {
-		table.AddRow(strconv.Itoa(task.Number), task.Name, task.Description, task.Status.ToString(), task.CreatedAt.Format(time.RFC1123))
+		datetimeFormatted := formatDatetime(task.CreatedAt)
+		table.AddRow(strconv.Itoa(task.Number), task.Name, task.Description, task.Status.ToString(), datetimeFormatted)
 	}
 	table.Render()
+}
+
+func formatDatetime(datetime time.Time) string {
+	switch {
+	case isToday(datetime):
+		return "Today"
+	case isYesterday(datetime):
+		return "Yesterday"
+	case isTomorrow(datetime):
+		return "Tomorrow"
+	default:
+		return datetime.Format(time.RFC1123)
+	}
+}
+
+func isToday(t time.Time) bool {
+	now := time.Now()
+	return now.Year() == t.Year() && now.Month() == t.Month() && now.Day() == t.Day()
+}
+
+func isYesterday(t time.Time) bool {
+	now := time.Now()
+	yesterday := now.AddDate(0, 0, -1)
+	return yesterday.Year() == t.Year() && yesterday.Month() == t.Month() && yesterday.Day() == t.Day()
+}
+
+func isTomorrow(t time.Time) bool {
+	now := time.Now()
+	tomorrow := now.AddDate(0, 0, 1)
+	return tomorrow.Year() == t.Year() && tomorrow.Month() == t.Month() && tomorrow.Day() == t.Day()
 }
